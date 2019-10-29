61006b36296e16632b61a8e5f04f438cebf78301
caio <you@yourdomain.example.com>
2019-10-29 00:53:55 -0200

    atualizando

--- src/main/java/com/caio/servicoCalc/Controller/ServicoCalcControler.java
+++ src/main/java/com/caio/servicoCalc/Controller/ServicoCalcControler.java
@@ -1,6 +1,8 @@
 package com.caio.servicoCalc.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +17,21 @@ public class ServicoCalcControler {
 	
 	final Counter counter = Metrics.counter("entity.count", "type", "order");
 	
+	@Autowired
+	Environment environment;
 
-	@GetMapping("/soma/{a}/{b}")
+	
+	
+
+	@GetMapping("/calc/soma/{a}/{b}")
 	public Retorno soma (@PathVariable String a, @PathVariable String b) {
 		counter.increment();
-		return new Retorno(Integer.parseInt(a)+Integer.parseInt(b));
+		return new Retorno(Integer.parseInt(a)+Integer.parseInt(b), environment.getProperty("local.server.port"));
 	}
-	
-	@GetMapping("/multiplica")
+	 
+	@GetMapping("/calc/multiplica") 
 	public Retorno multiplica (String a, String b) {
-		return new Retorno(Integer.parseInt(a)*Integer.parseInt(b));
+		return new Retorno(Integer.parseInt(a)*Integer.parseInt(b), environment.getProperty("local.server.port"));
 	}
 
 }
--- src/main/java/servicoCalc/pojo/Retorno.java
+++ src/main/java/servicoCalc/pojo/Retorno.java
@@ -3,9 +3,11 @@ package servicoCalc.pojo;
 public class Retorno {
 	
 	private int retorno;
+	private String servidor;
 
-	public Retorno(int i) {
+	public Retorno(int i, String s) {
 		this.retorno = i;
+		this.servidor = s;
 	}
 
 	public int getRetorno() {
@@ -14,6 +16,14 @@ public class Retorno {
 
 	public void setRetorno(int retorno) {
 		this.retorno = retorno;
+	}
+
+	public String getServidor() {
+		return servidor;
+	}
+
+	public void setServidor(String servidor) {
+		this.servidor = servidor;
 	}
 
 }
